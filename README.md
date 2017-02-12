fcb397017d6a24cbd8d278bc14a740882595156c
HelloWorld017 <deu07115@gmail.com>
2017-02-12 21:15:33 +0900

    Forked from casper, added some fonts, edited navigations

---
+++ README.md
@@ -0,0 +1,7 @@
+# Origami
+
+A fancy ghost theme.
+
+## Copyright & License
+
+Copyright (c) 2017 Ghost Foundation, if-Team - Released under the [MIT license](LICENSE).
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'src', 'index.js'),
+
+	output: {
+	  	path: path.resolve(__dirname, 'assets'),
+		publicPath: '/assets/',
+		filename: 'origami.bundle.js'
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				},
+				exclude: /node_modules/
+			},
+			{
+				test: /\.less$/,
+				loader: ExtractTextPlugin.extract({
+					use: [
+						'css-loader',
+						'less-loader'
+					],
+					fallback: 'style-loader'
+				})
+			},
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract({
+					use: 'css-loader',
+					fallback: 'style-loader'
+				})
+			},
+			{
+				test: /\.(png|jpg|gif|svg|ttf|woff|woff2|eot)$/,
+				loader: 'file-loader',
+				options: {
+					name: 'files/[name].[ext]?[hash]'
+				}
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractTextPlugin('origami.bundle.css'),
+		new webpack.ProvidePlugin({
+			"jQuery": "jquery"
+		})
+	],
+	devtool: '#eval-source-map'
+};
+
+if(process.env.NODE_ENV === 'production'){
+	module.exports.devtool = '#source-map';
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"'
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			sourceMap: true,
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.LoaderOptionsPlugin({
+			minimize: true
+		})
+	]);
+}
