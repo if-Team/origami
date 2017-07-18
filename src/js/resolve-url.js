42a47fa3203f5099c5271b9d6d30141bedd54904
Khinenw <deu07115@gmail.com>
2017-07-18 17:24:51 +0900

    Fixed wrong url

--- src/js/resolve-url.js
+++ src/js/resolve-url.js
@@ -1,8 +1,8 @@
 export default (url) => {
-	if(url.match(/^[^:]+:\/\//)) {
+	if(url.match(/^(?:[^:]+:)?\/\//)) {
 		return url;
 	}
-	
+
 	if(!url.startsWith('/')) url = `/${url}`;
 	if(window.origamiBaseUrl.endsWith('/')){
 		const base = window.origamiBaseUrl.slice(0, window.origamiBaseUrl.length - 1);
